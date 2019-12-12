cc9b0f8b88e2694c6cbde243568f54b29ea733e5
sigarashi <50ra4.dev.dev@gmail.com>
2019-12-12 21:38:59 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
+import * as serviceWorker from './serviceWorker';
 
 import './styles/main.scss';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+
 import config from './configuration/config';
+
+import { ErrorBoundary } from './components/others';
 
 Sentry.init(config.sentry);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ErrorBoundary isRoot={true}>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
