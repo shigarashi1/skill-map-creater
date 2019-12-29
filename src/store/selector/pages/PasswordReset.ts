bc4a2d16e595b18da927b57684c3ad4b6c9b0758
sigarashi <50ra4.dev.dev@gmail.com>
2019-12-29 19:42:46 +0900

    FIX: selector

--- src/store/selector/pages/PasswordReset.ts
+++ src/store/selector/pages/PasswordReset.ts
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect';
 import { AppState } from '../..';
 import { passwordResetPageReducers } from '../../pages/PasswordResetPage';
 
-const featureSelector = (state: AppState): ReturnType<typeof passwordResetPageReducers> => state.pages.signIn;
+const featureSelector = (state: AppState): ReturnType<typeof passwordResetPageReducers> => state.pages.passwordReset;
 
 export const PasswordResetPageSelector = {
   stateAll: createSelector(featureSelector, (state) => state),
