9dde086d19b8b886a32c0175c302a3d1df708344
S.Igarashi <50ra4.dev.dev@gmail.com>
2020-01-18 20:57:27 +0900

    FIX: npm modules

--- src/components/organisms/dialogs/index.ts
+++
@@ -1,6 +0,0 @@
-import ErrorDialog from './ErrorDialog/ErrorDialog';
-import InfoDialog from './InfoDialog/InfoDialog';
-import OkCancelDialog from './OkCancelDialog/OkCancelDialog';
-import YesNoDialog from './YesNoDialog/YesNoDialog';
-
-export { ErrorDialog, InfoDialog, OkCancelDialog, YesNoDialog };
